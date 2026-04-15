355fb499a428aab749bc7b37918802d24779f963
AlexanderJ11 <jordialexander1998@gmail.com>
2026-04-14 21:20:58 -0500

    Keny aprende Json

--- PRUEBAS/json_ejemplo.py
+++ PRUEBAS/json_ejemplo.py
@@ -1,11 +1,12 @@
 import json
 
-alumno = {
-    "nombre": "Alve",
-    "nota": 9,
-    "nombre1": "Yo",
-    "nota1": 10
-}
+alumno = [
+    { "nombre": "Jordi", "edad": 21, "notas":8,},
+    { "nombre": "Messi", "edad": 38, "notas": 7},
+    {"nombre": "CR7", "edad": 40, "notas": 5},
+    {"nombre": "Alexander", "edad": 17, "notas": 10},
+    {"nombre": "KENY", "edad": 11, "notas": 6.99},
+]
 
 
 # Guardar
@@ -17,5 +18,17 @@ with open("datos.json", "w") as archivo:
 with open("datos.json", "r") as archivo:
     datos = json.load(archivo)
 
-print(f"{datos['nombre']} tiene {datos['nota']}")
-print(f"{datos['nombre1']} tiene {datos['nota1']}")
+for alumnos in datos:
+    #VERIFICAR EDAD
+    if alumnos["edad"] >= 18:
+        estado = "ES MAYOR DE EDAD"
+    else:
+        estado = "ES MENOR DE EDAD"
+    #PROMEDIO DE NOTAS
+    if alumnos["notas"] >=6.99:
+        promedio = "SE ENCUENTRA APROBADO"
+    else:
+        promedio = "SE ENCUNETRTA REPROBADO"
+
+    print(f"{alumnos['nombre']} tiene {alumnos['edad']} años - {estado} con una nota de {alumnos["notas"]} - {promedio}")
+
